c7fe662cc821c7a4f039a30afab81ab736ca60c0
unknown <riteshjoshimca@gmail.com>
2019-10-07 16:30:21 +0530

    testing

---
+++ basic/baisc.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<body>
+<div ng-app="">
+  <p>Name : <input type="text" ng-model="name"></p>
+  <h1>Hello {{name}}</h1>
+</div>
+</body>
+<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.6.9/angular.min.js"></script>
+<script src="app.js"></script>
+</html>
